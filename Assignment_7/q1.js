8ad70ac85213a0631f5bead073b3116c6a2614e9
riyanshiluthra <riyanshi.luthra2023@vitstudent.ac.in>
2025-02-07 12:25:31 +0530

    Update q1.js

--- Assignment_7/q1.js
+++ Assignment_7/q1.js
@@ -1,9 +1,11 @@
 let number = Math.floor(Math.random() * 100) + 1;
 let guess = 0;
+let attempts = 0;
 
-while (guess !== number) {
+while (guess !== number && attempts < 5) {
   guess = parseInt(prompt("Guess the number between 1 and 100:"));
-  
+  attempts++;
+
   if (guess > number) {
     alert("Too high! Try again.");
   } else if (guess < number) {
@@ -11,4 +13,8 @@ while (guess !== number) {
   } else {
     alert("Congratulations! You've guessed the correct number.");
   }
+}
+
+if (guess !== number) {
+  alert("Sorry, you've used all 5 attempts. The correct number was " + number);
 }
